e92b4121e586d7e4ea05c8499546919948650737
Daniel Kluzner <daniel.kluzner@gmail.com>
2021-01-27 19:52:16 -0800

    add back to top button

--- image_script.js
+++ image_script.js
@@ -23,5 +23,30 @@ document.addEventListener("DOMContentLoaded",
             }
         );
 
+        // source: https://stackoverflow.com/questions/1145850/how-to-get-height-of-entire-document-with-javascript
+        // for getting page height
+        var body = document.body;
+        var html = document.documentElement;
+        var height = Math.max(body.scrollHeight, body.offsetHeight, 
+                    html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+        // source: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+        // used for going back to the top of page
+        document.getElementById("back_to_top_bttn").addEventListener("click",
+            function(event) {
+                document.body.scrollTop = 0;
+                document.documentElement.scrollTop = 0;
+            }
+        );
+
+        window.addEventListener("scroll",
+            function(event){
+                if (document.body.scrollTop > height/4 || document.documentElement.scrollTop > height/4) {
+                    document.getElementById("back_to_top_bttn").style.display = "flex";
+                } else {
+                    document.getElementById("back_to_top_bttn").style.display = "none";
+                }
+            }
+        );
     }
 );
