a779c5958c3e23cad5b49f6d32a69734a071f5df
Daniel Kluzner <daniel.kluzner@gmail.com>
2021-01-27 19:52:36 -0800

    add back to top bttn to videos

--- video_script.js
+++ video_script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded",
     function(event){
 
         // source: https://stackoverflow.com/questions/5235145/changing-source-on-html5-video-tag/5371478#5371478
-        // ONLY FOR THE setAttribute() line.
+        // ONLY for the setAttribute() line.
         const vids = document.getElementsByClassName("child");
         for(let vid of vids){
             vid.addEventListener("click",
@@ -25,6 +25,7 @@ document.addEventListener("DOMContentLoaded",
         );
 
         // source: https://gist.github.com/cferdinandi/9044694
+        // used to stop the video playing in the overlay
         function stopVideo(){
             var iframe = document.querySelector('#overlay');
             var video = document.querySelector('video');
@@ -47,5 +48,30 @@ document.addEventListener("DOMContentLoaded",
             }
         );
 
+        // source: https://stackoverflow.com/questions/1145850/how-to-get-height-of-entire-document-with-javascript
+        // for getting page height
+        var body = document.body;
+        var html = document.documentElement;
+        var height = Math.max(body.scrollHeight, body.offsetHeight, 
+                    html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+        // source: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+        // used for going back to the top of page
+        document.getElementById("back_to_top_bttn").addEventListener("click",
+            function(event) {
+                document.body.scrollTop = 0;
+                document.documentElement.scrollTop = 0;
+            }
+        );
+
+        window.addEventListener("scroll",
+            function(event){
+                if (document.body.scrollTop > height/4 || document.documentElement.scrollTop > height/4) {
+                    document.getElementById("back_to_top_bttn").style.display = "flex";
+                } else {
+                    document.getElementById("back_to_top_bttn").style.display = "none";
+                }
+            }
+        );
     }
 );
